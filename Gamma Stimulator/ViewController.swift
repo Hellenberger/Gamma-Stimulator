29c4c207854d0254b7eb7711e7a07352d009e341
Howard Ellenberger <Hellenberger@yahoo.com>
2024-03-02 09:40:35 -0800

    Initial Commit

---
+++ Gamma Stimulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gamma Stimulator
+//
+//  Created by Howard Ellenberger on 3/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
